4cd7e8d72a13f841c279e93ae36c3aec73ea294d
Moyashi911 <sabanomisoni911@gmail.com>
2024-04-30 22:10:55 +0900

    画像追加

--- templates/static/script/menu.js
+++ templates/static/script/menu.js
@@ -1,15 +1,23 @@
 var orderNum = 0;
 var btn1 = document.getElementById("menu1");
-var orderbtn = document.getElementById("orderbtn");
+var btn2 = document.getElementById("menu2");
+var btn3 = document.getElementById("menu3");
+var btn4 = document.getElementById("menu4");
+var selected = document.getElementById("selectedmenu");
+var menuform = document.getElementById('menuname');
 btn1.addEventListener("click", function () {
-  var selected = document.getElementById("selectedmenu");
-  var menuform = document.getElementById('menuname');
   selected.src = "../static/img/dish1.jpg";
-  menuform.value = "menu1"
+  menuform.value = "スープ"
 });
-// orderbtn.addEventListener("click", function () {
-//   var selected = document.getElementById("selectedmenu");
-//   var selected_menuname = document.getElementById("selected_menuname");
-//   selected.src = "../static/img/selectmenu.png";
-//   // selected_menuname.innerHTML = "";
-// });
+btn2.addEventListener("click", function () {
+  selected.src = "../static/img/dish2.jpg";
+  menuform.value = "カレー"
+});
+btn3.addEventListener("click", function () {
+  selected.src = "../static/img/dish3.jpg";
+  menuform.value = "唐揚げ"
+});
+btn4.addEventListener("click", function () {
+  selected.src = "../static/img/dish4.jpg";
+  menuform.value = "ポテト"
+});
